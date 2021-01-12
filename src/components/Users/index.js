42e3411ccfa969a9a83b95f6c138f2419922a517
Sleather Vega <sleatherv28@hotmail.com>
2021-01-11 21:00:36 -0500

    [ADD] Loading spinner component

--- src/components/Users/index.js
+++ src/components/Users/index.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
+import Spinner from '../General/Spinner';
 
 import * as usersActions from '../../actions/usersActions';
 class Users extends Component {
     componentDidMount() {
         this.props.getAll();
+    }
+    setContent = () => {
+        if (this.props.loading) {
+            return <Spinner />
+        }
+        return (
+            <table className="tabla">
+                <thead>
+                    <tr>
+                        <th>
+                            Nombre
+                            </th>
+                        <th>
+                            Correo
+                            </th>
+                        <th>
+                            Enlace
+                            </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.setRows()}
+                </tbody>
+            </table>
+
+
+        )
     }
     setRows = () => (
         this.props.users.map((user) => (
@@ -18,24 +46,7 @@ class Users extends Component {
     render() {
         return (
             <div>
-                <table className="tabla">
-                    <thead>
-                        <tr>
-                            <th>
-                                Nombre
-                            </th>
-                            <th>
-                                Correo
-                            </th>
-                            <th>
-                                Enlace
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.setRows()}
-                    </tbody>
-                </table>
+                {this.setContent()}
             </div>
         );
     }
