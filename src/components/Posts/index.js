44158e87eae858b9e2c90f1abd6e8c5f9f5faffb
Sleather Vega <sleatherv28@hotmail.com>
2021-01-14 22:21:44 -0500

    Sharing Reducer with Posts component

--- src/components/Posts/index.js
+++ src/components/Posts/index.js
@@ -1,14 +1,25 @@
 
 import React, { Component } from 'react'
-
+import { connect } from 'react-redux';
+import * as usersActions from '../../actions/usersActions';
 class Posts extends Component {
+    componentDidMount() {
+        if (!this.props.users.length) {
+            this.props.getAll();
+        }
+    }
     render() {
         return (
             <div>
+                <h1>Posts</h1>
                 {this.props.match.params.key}
             </div>
         )
     }
 }
 
-export default Posts;
+const mapStateToProps = (reducers) => {
+    return reducers.usersReducer;
+}
+
+export default connect(mapStateToProps, usersActions)(Posts);
