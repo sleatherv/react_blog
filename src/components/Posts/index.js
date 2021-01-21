8f37e0b5a1b93b0e64a3b515af154a1bcdb511ec
Sleather Vega <sleatherv28@hotmail.com>
2021-01-20 22:21:46 -0500

    ADD Loading and Error validations in Posts

--- src/components/Posts/index.js
+++ src/components/Posts/index.js
@@ -1,8 +1,12 @@
 
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
+import Sinner from '../General/Spinner';
+import Fatal from '../General/Fatal';
+
 import * as usersActions from '../../actions/usersActions';
 import * as postsActions from '../../actions/postsActions';
+import Spinner from '../General/Spinner';
 
 const { getAll: getAllUsers } = usersActions;
 const { getPostsByUser: getPostsByUser } = postsActions;
@@ -17,15 +21,33 @@ class Posts extends Component {
         if (!this.props.usersReducer.users.length) {
             await this.props.getAllUsers();
         }
+        if (this.props.usersReducer.error) {
+            return;
+        }
         if (!('posts_key' in this.props.usersReducer.users[key])) {
             this.props.getPostsByUser(key);
         }
     }
+    setUser = () => {
+        const {
+            usersReducer,
+            match: { params: { key } },
+        } = this.props;
+        if (usersReducer.error) {
+            return <Fatal message={usersReducer.error} />
+        }
+        if (!usersReducer.users.length || usersReducer.loading) {
+            return <Spinner />
+        }
+        const name = usersReducer.users[key].name;
+
+        return <h1>Posts by {name}</h1>;
+    };
     render() {
         console.log(this.props)
         return (
             <div>
-                <h1>Posts</h1>
+                {this.setUser()}
                 {this.props.match.params.key}
             </div>
         )
