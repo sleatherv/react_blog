93397588d5b01bc8bb6ab0bd80f6750226af88b6
Sleather Vega <sleatherv28@hotmail.com>
2021-02-05 22:44:13 -0500

    handling inputs using redux

--- src/components/Tasks/Save.js
+++ src/components/Tasks/Save.js
@@ -1,26 +1,51 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from "react-redux";
+import * as tasksActions from '../../actions/tasksActions';
 
 class Save extends Component {
+
+    changeUserId = (event) => {
+        this.props.changeUserId(event.target.value)
+    }
+
+    changeTitle = (e) => {
+        this.props.changeTitle(e.target.value);
+    }
     render() {
         return (
             <>
                 <h1>
                     Save Task
                 </h1>
-                User id:
-                <input type='number' />
-                <br />
-                <br />
-                Title:
-                <input type="text" />
-                <br />
-                <br />
-                <button>
-                    Save
-                </button>
+                <form action="">
+                    <label htmlFor="user_id">
+                        <span>User id:</span>
+                        <input
+                            type='number'
+                            value={this.props.user_id}
+                            onChange={this.changeUserId}
+                            id='user_id' />
+                    </label>
+                    <br /> <br />
+                    <label htmlFor="title">
+                        <span>Title:</span>
+                        <input
+                            type="text"
+                            value={this.props.title}
+                            onChange={this.changeTitle}
+                            id='title' />
+                    </label>
+                    <br />
+                    <br />
+                    <button>
+                        Save
+                    </button>
+                </form>
             </>
         )
     }
 }
 
-export default Save;
+const mapStateToProps = ({ tasksReducer }) => tasksReducer;
+
+export default connect(mapStateToProps, tasksActions)(Save);
--- src/reducers/tasksReducer.js
+++ src/reducers/tasksReducer.js
@@ -3,7 +3,9 @@ import { GET_ALL_TASKS, LOADING, ERROR } from "../types/tasksTypes";
 const INITIAL_STATE = {
     tasks: {},
     loading: false,
-    error: ''
+    error: '',
+    user_id: '',
+    title: ''
 };
 
 const tasksReducer = (state = INITIAL_STATE, action) => {
@@ -17,8 +19,15 @@ const tasksReducer = (state = INITIAL_STATE, action) => {
             };
         case LOADING:
             return { ...state, loading: true };
+
         case ERROR:
             return { ...state, error: action.payload, loading: false };
+
+        case 'change_user_id':
+            return { ...state, user_id: action.payload };
+
+        case 'change_title':
+            return { ...state, title: action.payload }
         default:
             return state;
     }
